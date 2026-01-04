2db1b525bb37d1cec6ccfdf31ab310122b459b6f
ShubhamThakur001 <shubhamthakurganesh@gmail.com>
2026-01-04 21:59:30 +0530

    learn decorator

--- 04_functions/decorators.py
+++ 04_functions/decorators.py
@@ -62,23 +62,23 @@ greet("Shubham",greeting="hey !")
 Problem : Cache return values
     implement a decorator that caches the return values of a function,so that when it's called with the same arguments , the cached values is returned instead of re-executing the functions
 """
-def cache(func):
-    cache_value = {}
-    def wrapper(*args):
-        if args in cache_value:
-            return cache_value[args]
-        result=func(*args)
-        cache_value[args]=result
-        return result
-    return wrapper
+# def cache(func):
+#     cache_value = {}
+#     def wrapper(*args):
+#         if args in cache_value:
+#             return cache_value[args]
+#         result=func(*args)
+#         cache_value[args]=result
+#         return result
+#     return wrapper
 
 
-@cache
-def long_running_function(a,b):
-    time.sleep(5)
-    return a + b
+# @cache
+# def long_running_function(a,b):
+#     time.sleep(5)
+#     return a + b
 
-print(long_running_function(2,3))
-print(long_running_function(2,3))
-print(long_running_function(5,3))
+# print(long_running_function(2,3))
+# print(long_running_function(2,3))
+# print(long_running_function(5,3))
 
