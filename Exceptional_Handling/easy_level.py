c16bc922685c01455ca61bed0cfd4cbd89374fe5
ShubhamThakur001 <shubhamthakurganesh@gmail.com>
2026-01-06 21:15:36 +0530

    learn Exceptional_Handling

--- Exceptional_Handling/easy_level.py
+++ Exceptional_Handling/easy_level.py
@@ -2,60 +2,60 @@
  1️⃣ Age Verification System
 
     Create a custom exception UnderAgeError.
-    Raise it if the user age is below 18 and handle it gracefully.
+#     Raise it if the user age is below 18 and handle it gracefully.
 
-class InvalidAgeError(Exception):
-    pass
+# class InvalidAgeError(Exception):
+#     pass
 
-try:
-    age = int(input("Enter Your Age :"))
-    if age < 18:
-        raise InvalidAgeError("Age Must be 18+")
-    print("done...")
-except (InvalidAgeError,ValueError) as e:
-    print("Error" , e)
-"""
+# try:
+#     age = int(input("Enter Your Age :"))
+#     if age < 18:
+#         raise InvalidAgeError("Age Must be 18+")
+#     print("done...")
+# except (InvalidAgeError,ValueError) as e:
+#     print("Error" , e)
+# 
 
-"""
-    2️⃣ Password Strength Checker
+# """
+#     2️⃣ Password Strength Checker
 
-    Create WeakPasswordError.
-    Raise it when the password is less than 8 characters or contains no digit.
-
-
-def validate_password(password):
-    if len(password) < 8:
-        raise WeakPasswordError("Min 8 chars required")
-    if not any(c.isdigit() for c in password):
-        raise WeakPasswordError("At least 1 digit required")
-
-try:
-    validate_password(input("Enter password: "))
-    print("Password accepted")
-except WeakPasswordError as e:
-    print("Validation failed:", e)
-
-"""
+#     Create WeakPasswordError.
+#     Raise it when the password is less than 8 characters or contains no digit.
 
 
-"""
+# def validate_password(password):
+#     if len(password) < 8:
+#         raise WeakPasswordError("Min 8 chars required")
+#     if not any(c.isdigit() for c in password):
+#         raise WeakPasswordError("At least 1 digit required")
 
-    3️⃣ Simple ATM Withdrawal
+# try:
+#     validate_password(input("Enter password: "))
+#     print("Password accepted")
+# except WeakPasswordError as e:
+#     print("Validation failed:", e)
 
-    Create InsufficientBalanceError.
-    Raise it if withdrawal amount is greater than account balance.
+# """
 
-class InsufficientBalanceError(Exception):
-    pass
 
-balance = 5000
-withdraw = 6000
+# """
 
-try:
-    if withdraw > balance:
-        raise InsufficientBalanceError("Insufficient Balance")
-    balance -= withdraw
-    print("withdraw sucessfully")
-except InsufficientBalanceError as e:
-    print("Error ",e)
-"""
+#     3️⃣ Simple ATM Withdrawal
+
+#     Create InsufficientBalanceError.
+#     Raise it if withdrawal amount is greater than account balance.
+
+# class InsufficientBalanceError(Exception):
+#     pass
+
+# balance = 5000
+# withdraw = 6000
+
+# try:
+#     if withdraw > balance:
+#         raise InsufficientBalanceError("Insufficient Balance")
+#     balance -= withdraw
+#     print("withdraw sucessfully")
+# except InsufficientBalanceError as e:
+#     print("Error ",e)
+# """
